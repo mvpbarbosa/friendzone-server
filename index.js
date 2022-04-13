ef6deb8358394d4af226c4354d5bbe296b0a8f40
mvpbarbosa <97544652+mvpbarbosa@users.noreply.github.com>
2022-04-12 22:55:58 -0300

    Rotas getAll e getById; instalação do nodemon

--- index.js
+++ index.js
@@ -7,39 +7,45 @@ app.use(express.json());
 
 //Definir dados para usar na aplicação
 const paletas = [
-    {
-        id: 1,
-        sabor: 'Açaí com Leite Condensado',
-        descricao: 'Açaí com Leite Condensado',
-        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-        preco: 10.0
-    },
-    {
-        id: 2,
-        sabor: 'Banana com Nutella',
-        descricao: 'Banana com Nutella',
-        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-        preco: 10.0
-    },
-    {
-        id: 3,
-        sabor: 'Chocolate Belga',
-        descricao: 'Chocolate Belga',
-        foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
-        preco: 7.0
-    },
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao: 'Açaí com Leite Condensado',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao: 'Banana com Nutella',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao: 'Chocolate Belga',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 7.0,
+  },
 ];
 
 app.get('/', function (req, res) {
   res.send('Hello Blue Módulo 3 Fullstack');
 });
 
-app.get('/paletas/find-paletas', (req, res) =>{
-  res.send(paletas)
-})
+// Get all
+app.get('/paletas/todas-paletas', (req, res) => {
+  res.send(paletas);
+});
+
+// Get by id
+app.get('/paletas/paleta/:id', (req, res) => {
+  const parametroId = Number(req.params.id);
+  const escolhaPaleta = paletas.find(paleta => paleta.id === parametroId);
+  res.send(escolhaPaleta);
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port} 🚀`);
 });
-
-
