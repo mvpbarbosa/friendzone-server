fa9a008a2bfd6485065f429e849ee708301bf39f
mvpbarbosa <97544652+mvpbarbosa@users.noreply.github.com>
2022-04-26 21:29:02 -0300

    Alter conection string - mongo atlas

--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = () => {
   mongoose
-    .connect("mongodb://localhost:27017/publicacoes-db", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://root:admin@api-friendzone.tsya0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("MongoDb CONNECT"))
     .catch((error) =>
       console.log(`Erro ao conectar com o MongoDB, erro: ${error}`)
